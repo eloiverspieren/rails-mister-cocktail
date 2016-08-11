836fd18cec3e8e187a025c27885a272641c413df
eloi verspieren <eloi.verspieren@gmail.com>
2016-08-11 11:15:18 +0200

    css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+
+  get 'doses/show'
+
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/edit'
+
+  get 'doses/update'
+
+  get 'doses/destroy'
+
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'ingredients/new'
+
+  get 'ingredients/create'
+
+  get 'ingredients/edit'
+
+  get 'ingredients/update'
+
+  get 'ingredients/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/update'
+
+  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
