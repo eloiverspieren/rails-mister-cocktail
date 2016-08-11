957d841658d7f4151638f39e564997b734b7dcb1
eloi verspieren <eloi.verspieren@gmail.com>
2016-08-11 16:07:01 +0200

    pp

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :destroy]
   end
 
+root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
